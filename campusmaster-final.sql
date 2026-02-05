859c317ec31a04793a61451f3560033627068d09
tourecodeur <touremacoumba682@gmail.com>
2026-02-05 13:45:36 +0000

    mise a jour bdd

--- campusmaster-final.sql
+++ campusmaster-final.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 05 déc. 2025 à 18:41
+-- Généré le : jeu. 05 fév. 2026 à 14:23
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -37,6 +37,14 @@ CREATE TABLE `cours` (
   `semestre_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `cours`
+--
+
+INSERT INTO `cours` (`id`, `date_creation`, `description`, `titre`, `enseignant_id`, `matiere_id`, `semestre_id`) VALUES
+(1, '2025-12-25 16:39:23.000000', 'Initiation à la programmation', 'Programmation', 3, 1, 1),
+(2, '2025-12-29 12:31:47.000000', 'Initiation à l\'algo', 'Algorithme', 3, 1, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -52,6 +60,13 @@ CREATE TABLE `depots_devoirs` (
   `etudiant_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `depots_devoirs`
+--
+
+INSERT INTO `depots_devoirs` (`id`, `date_depot`, `url_fichier`, `version`, `devoir_id`, `etudiant_id`) VALUES
+(1, '2025-12-25 16:49:12.000000', 'https://github.com/', 0, 1, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -66,6 +81,14 @@ CREATE TABLE `devoirs` (
   `cours_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `devoirs`
+--
+
+INSERT INTO `devoirs` (`id`, `consigne`, `date_limite`, `titre`, `cours_id`) VALUES
+(1, 'réaliser le td 1 POO', '2025-12-25 16:45:00.000000', 'TD 1', 1),
+(2, 'Deadline dans 2 jours', '2026-01-01 19:03:00.000000', 'TD 2', 2);
+
 -- --------------------------------------------------------
 
 --
@@ -79,6 +102,13 @@ CREATE TABLE `discussions` (
   `cours_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `discussions`
+--
+
+INSERT INTO `discussions` (`id`, `date_creation`, `sujet`, `cours_id`) VALUES
+(1, '2025-12-25 16:56:56.000000', 'Depôt devoir', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -90,6 +120,13 @@ CREATE TABLE `matieres` (
   `libelle` varchar(255) NOT NULL,
   `module_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `matieres`
+--
+
+INSERT INTO `matieres` (`id`, `libelle`, `module_id`) VALUES
+(1, 'poo', 1);
 
 -- --------------------------------------------------------
 
@@ -104,6 +141,13 @@ CREATE TABLE `messages_discussion` (
   `auteur_id` bigint(20) DEFAULT NULL,
   `discussion_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `messages_discussion`
+--
+
+INSERT INTO `messages_discussion` (`id`, `contenu`, `date_envoi`, `auteur_id`, `discussion_id`) VALUES
+(1, 'veuillez rendre le TD 1 avant jeudi', '2025-12-25 16:59:10.000000', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -120,6 +164,17 @@ CREATE TABLE `messages_prives` (
   `expediteur_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `messages_prives`
+--
+
+INSERT INTO `messages_prives` (`id`, `contenu`, `date_envoi`, `tag`, `destinataire_id`, `expediteur_id`) VALUES
+(1, 'jdjhggjkgjkgjkg', '2026-01-02 20:25:18.000000', 'PRIVE', 6, 2),
+(2, 'bjr', '2026-01-02 20:40:22.000000', 'PRIVE', 1, 3),
+(3, 'Bonsoir Mousieur, vous avez l\'autorisation de démarrer les cours', '2026-01-19 17:31:50.000000', 'PRIVE', 1, 2),
+(4, 'Bonsoir, vous pouvez débuter les cours', '2026-02-02 21:04:35.000000', 'PRIVE', 3, 2),
+(5, 'D\'accord', '2026-02-02 21:09:17.000000', 'PRIVE', 2, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -131,6 +186,13 @@ CREATE TABLE `modules` (
   `code` varchar(255) NOT NULL,
   `libelle` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `modules`
+--
+
+INSERT INTO `modules` (`id`, `code`, `libelle`) VALUES
+(1, '1234', 'POO');
 
 -- --------------------------------------------------------
 
@@ -144,6 +206,13 @@ CREATE TABLE `notes_devoirs` (
   `note` double DEFAULT NULL,
   `depot_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `notes_devoirs`
+--
+
+INSERT INTO `notes_devoirs` (`id`, `commentaire`, `note`, `depot_id`) VALUES
+(1, 'Trés bien', 17, 1);
 
 -- --------------------------------------------------------
 
@@ -160,6 +229,16 @@ CREATE TABLE `notifications` (
   `utilisateur_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `notifications`
+--
+
+INSERT INTO `notifications` (`id`, `date_creation`, `lu`, `message`, `type_evenement`, `utilisateur_id`) VALUES
+(1, '2025-12-25 17:02:22.000000', b'1', 'Les examens debuterons la semain prochaine', 'Examen semestre 1', 1),
+(2, '2025-10-19 20:20:00.000000', b'1', 'Léa a déposé son devoir \"Calculateur IMC en JS\".', 'DEPOT_DEVOIR', 2),
+(3, '2025-10-25 09:00:00.000000', b'0', 'Nouveau support de cours disponible : \"ML Fondamentaux\".', 'NOUVEAU_SUPPORT', 5),
+(4, '2025-10-26 14:25:00.000000', b'0', 'Thomas vous a envoyé un message privé.', 'MESSAGE_PRIVE', 3);
+
 -- --------------------------------------------------------
 
 --
@@ -171,6 +250,13 @@ CREATE TABLE `semestres` (
   `code` varchar(255) NOT NULL,
   `description` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `semestres`
+--
+
+INSERT INTO `semestres` (`id`, `code`, `description`) VALUES
+(1, 'S1', '2024 - 2025');
 
 -- --------------------------------------------------------
 
@@ -188,6 +274,17 @@ CREATE TABLE `supports_cours` (
   `cours_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `supports_cours`
+--
+
+INSERT INTO `supports_cours` (`id`, `date_upload`, `nom_fichier`, `type`, `url_fichier`, `version`, `cours_id`) VALUES
+(1, '2025-12-25 16:43:03.000000', 'POO -S1', 'PDF', 'https://ton-site.com/devoirs/depot-10.zip', 1, 1),
+(2, '2025-12-29 13:44:55.000000', 'Séance 1', 'AUTRE', 'https://github.com/', 1, 1),
+(3, '2025-12-29 16:45:45.000000', 'Cours 1', 'AUTRE', 'https://github.com/', 1, 2),
+(8, '2025-12-30 15:31:32.000000', 'Cours 2', 'PDF', 'https://openclassrooms.com/', 1, 2),
+(14, '2025-12-30 21:07:21.000000', 'Cours 1', 'PDF', 'gitii.com', 1, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -203,6 +300,18 @@ CREATE TABLE `utilisateurs` (
   `nom_complet` varchar(255) NOT NULL,
   `role` enum('ADMIN','ENSEIGNANT','ETUDIANT') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `actif`, `date_inscription`, `email`, `mot_de_passe`, `nom_complet`, `role`) VALUES
+(1, b'1', '2025-12-25 14:26:38.000000', 'etudiant1@campus.com', '$2a$10$2h.Xp6MjilGCWtx63bTdheWUpBsEJ8yEwYEYrzoCMMoordJpCUP0m', 'Etudiant Test', 'ETUDIANT'),
+(2, b'1', '2025-12-25 15:46:24.000000', 'admin1@campus.com', '$2a$10$kvGWnlAuGwVyYqA9GCeYvuIoBTOgsOLT/yqu0DFO6jBZ1aiaETmFC', 'Admin Test', 'ADMIN'),
+(3, b'1', '2025-12-25 15:49:03.000000', 'enseignant1@campus.com', '$2a$10$sA4vaP9BWNWANZ5r/3mvsuieWfR3wPfMHj3tXLY3XUHa07zTAQcIS', 'Enseignant Test', 'ENSEIGNANT'),
+(4, b'1', '2025-12-27 13:41:18.000000', 'toure1@campus.com', '$2a$10$oHYnTZPo3bT.bxlGtpBk/Owi1kk5KcLYbLloubT.EgbFjSRLVnmqO', 'Macoumba Touré', 'ETUDIANT'),
+(5, b'1', '2025-12-27 13:58:46.000000', 'khadim1@campus.com', '$2a$10$oWSm/DmZw5CJxkea2nPMw.5PyGO3P9No1CYJq4ZSyl1i2ZYIfvenC', 'Khadim Diop', 'ENSEIGNANT'),
+(6, b'1', '2026-01-02 20:17:22.000000', 'talla@campus.com', '$2a$10$E.sAno032lNM51qUWRO3v.T7gE//Ti2DjfPgc8OMUiWeEd52ybDyy', 'talla diop', 'ETUDIANT');
 
 --
 -- Index pour les tables déchargées
@@ -310,79 +419,79 @@ ALTER TABLE `utilisateurs`
 -- AUTO_INCREMENT pour la table `cours`
 --
 ALTER TABLE `cours`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `depots_devoirs`
 --
 ALTER TABLE `depots_devoirs`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `devoirs`
 --
 ALTER TABLE `devoirs`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `discussions`
 --
 ALTER TABLE `discussions`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `matieres`
 --
 ALTER TABLE `matieres`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `messages_discussion`
 --
 ALTER TABLE `messages_discussion`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `messages_prives`
 --
 ALTER TABLE `messages_prives`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `modules`
 --
 ALTER TABLE `modules`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `notes_devoirs`
 --
 ALTER TABLE `notes_devoirs`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `semestres`
 --
 ALTER TABLE `semestres`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `supports_cours`
 --
 ALTER TABLE `supports_cours`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Contraintes pour les tables déchargées
